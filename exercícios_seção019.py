cc752d6c385abd5cc8da7764c2edb4c62e076df8
rafa-fortes <r.s.neves@hotmail.com>
2022-02-28 16:51:46 -0300

    exercício_seção019

---
+++ exercícios_seção019.py
@@ -0,0 +1,10 @@
+n = int(input('Digite um número inteiro: '))
+
+if n % 3 == 0:
+    print(f'O {n} é divisível por 3 ')
+elif n % 5 == 0:
+    print(f'O {n} é divisível por 5 ') 
+else: 
+    print(f'O {n} não divisível por nem 3 nem por 5 ')       
+
+
